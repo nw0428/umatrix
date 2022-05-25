73a466d34a6d57772ba87c38fd24df48e26289c2
David Zabner <david.zabner@gmail.com>
2022-05-25 12:53:43 -0400

    Can't get the mpy files to work

---
+++ installer/base_script.py
@@ -0,0 +1,52 @@
+# Paste this script in an empty Python project
+# Run once to install
+# Warning: it can take a while to download and start. Be patient.
+# Watch console output to check progress.
+# After reboot DO NOT LET THE SOFTWARE UPDATE YOUR HUB
+# Works with the LEGO SPIKE app and the LEGO MINDSTORMS app
+
+from ubinascii import hexlify, a2b_base64
+from uhashlib import sha256
+from os import mkdir
+
+
+encoded={}
+
+def calc_hash(b):
+    return hexlify(sha256(b).digest()).decode()
+
+error=False
+# try:
+#     mkdir('/projects/mpy_robot_tools')
+# except:
+#     pass
+
+for file, code, hash_gen in encoded:
+    print("Writing file ", file)
+    # hash_gen=code[1]
+    target_loc = '/'+file
+    
+    print('writing '+file+' to folder /')
+    with open(target_loc,'wb') as f:
+        for chunk in code:
+            f.write(a2b_base64(chunk))
+    del code
+
+    try:
+        print('Finished writing '+file+', Checking hash.')
+        result=open(target_loc,'rb').read()
+        hash_check=calc_hash(result)
+
+        print('Hash generated: ',hash_gen)
+
+        if hash_check != hash_gen:
+            print('Failed hash of .mpy on SPIKE: '+hash_check)
+            error=True
+    except Exception as e:
+        print(e)
+
+
+if not error:
+    print('Library written succesfully. Reset your hub.')
+else:
+    print('Failure in writing library!')
---
+++ installer/create_install_script.py
@@ -0,0 +1,44 @@
+#!python3
+
+# Run this on a Mac or Linux machine to create/update 'install_uartremote.py'
+# Copy the contents of install_uartremote.py into an empty SPIKE Prime project
+# And run to install
+
+import binascii, mpy_cross, time
+import hashlib
+import os
+from functools import partial
+
+LIB = '../'
+MPY_LIB = '../mpy/'
+INSTALLER = 'install_mpy_matrix_tools.py'
+BASE_SCRIPT = 'base_script.py'
+SKIP_FILES = ['__pycache__', 'matrix.py']
+CHUNK_SIZE = 2**8
+
+files = [f for f in os.listdir(LIB) if f not in SKIP_FILES and ".py" in f ]
+encoded = []
+
+for f in files:
+    out_file = f.split(".")[0]+".mpy"
+    out_file_loc = MPY_LIB+out_file
+    mpy_cross.run('-march=armv6',LIB+f,'-o', out_file_loc)
+    time.sleep(0.5)
+    with open(out_file_loc,'rb') as mpy_file:
+        file_hash = hashlib.sha256(mpy_file.read()).hexdigest()
+    chunks = []
+    with open(out_file_loc,'rb') as mpy_file:
+        for chunk in iter(partial(mpy_file.read, CHUNK_SIZE), b''):
+            chunks += [binascii.b2a_base64(chunk).decode('utf-8')]
+    print(out_file,": ",len(chunks)," chunks of ", CHUNK_SIZE)
+    encoded += [(
+        out_file,
+        tuple(chunks), 
+        file_hash
+    )]
+
+
+spike_code=open(BASE_SCRIPT,'r').read()
+
+with open(INSTALLER,'w') as f:
+     f.write(spike_code.format(repr(tuple(encoded))))
